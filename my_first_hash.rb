3f760dd285a19045c5232f824574fa1fa74e4a17
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-05 23:28:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   animals_I_have = {
-    "dogs" => 2
+    "dogs" => 2,
     "goats" => 6
 }
 
